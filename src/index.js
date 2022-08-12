271cd7e5d3d2435db034e18c21d7a6a9ceb3f1bb
Yauheniya Tarasenka <yauheniya_tarasenka@epam.com>
2022-08-12 21:22:59 +0300

    solving task with brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPairs = {}
+  bracketsConfig.forEach(el => {
+    bracketsPairs[el[1]]=el[0];
+  })
+  let stack = [];
+  for (let i=0; i<str.length; i++) {
+    let currentBracket = str[i];
+    let topElement = stack[stack.length - 1];
+    if (bracketsPairs[currentBracket] === topElement && stack.length !== 0){
+      stack.pop();
+    } else {
+      stack.push(currentBracket);
+    }
+  }
+  return stack.length === 0;
 }
+
+
+  
+  
+    
+
+
+
+
+
+
+  
